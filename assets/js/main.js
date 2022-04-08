7592694940c1d4e8ab743f131a84165d4e172e36
LAPTOP-5HC9EN9R\bingxue <91114bingxue>
2022-04-08 11:04:10 +0800

    v1.0 test 220408

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+/**
+ * Template Name: WeBuild - v4.6.1
+ * Template URL: https://bootstrapmade.com/free-bootstrap-coming-soon-template-countdwon/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function() {
+    "use strict";
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Countdown timer
+     */
+
+    let countdown = select('.countdown');
+
+    const countDownDate = function() {
+        let timeleft = new Date().getTime() - new Date(countdown.getAttribute('data-count')).getTime();
+
+        let weeks = Math.floor(timeleft / (1000 * 60 * 60 * 24 * 7));
+        let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+        let output = countdown.getAttribute('data-template');
+        output = output.replace('%w', weeks).replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
+        countdown.innerHTML = output;
+    }
+    countDownDate();
+    setInterval(countDownDate, 1000);
+
+})()
+
+
+
+/*--------------------------------------------------------------
+# text moving https://tobiasahlin.com/moving-letters/
+--------------------------------------------------------------*/
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml11 .line',
+        scaleY: [0, 1],
+        opacity: [0.5, 1],
+        easing: "easeOutExpo",
+        duration: 700
+    })
+    .add({
+        targets: '.ml11 .line',
+        translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+        easing: "easeOutExpo",
+        duration: 700,
+        delay: 100
+    }).add({
+        targets: '.ml11 .letter',
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=775',
+        delay: (el, i) => 34 * (i + 1)
+    }).add({
+        targets: '.ml11',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
